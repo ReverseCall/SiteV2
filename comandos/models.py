fc16798b1c568b558d7bd881d195dcf6bd24de40
ReverseCall <r.alisson123987@gmail.com>
2023-11-13 17:18:16 -0300

    v2

--- comandos/models.py
+++ comandos/models.py
@@ -3,12 +3,19 @@ from django.db import models
 # Create your models here.
 
 class Mensagem(models.Model):
+    TIPO_CHOICE = [
+        ('dica', 'Dica'),
+        ('informacao', 'Informação'),
+    ]
+
     conteudo = models.TextField()
     dia_envio = models.DateField()
     horario_envio = models.TimeField()
+    tipo_mensagem = models.CharField(max_length=20, choices=TIPO_CHOICE, default='informacao')
     ultima = models.BooleanField(default=False)
     mensagem_adicional = models.TextField(blank=True, null=True)
     audio_url = models.FileField(upload_to='audios/', null=True, blank=True)
+
 
     def __str__(self):
         return self.conteudo
